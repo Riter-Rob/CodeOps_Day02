27468caf6de86a1fc8ed855bf117145d238f1d10
Riter-Rob <robeltesfayee27@gmail.com>
2026-07-12 14:24:49 -0700

    first commit

---
+++ customer_report.py
@@ -0,0 +1,37 @@
+customers = [ 
+    ("Almaz", 1500), 
+    ("Dawit", 700), 
+    ("Tigist", 200), 
+    ("Hanna", 1200), 
+    ("Samuel", 450), 
+] 
+
+def tier(balance): 
+    """Determines customer tier based on their TeleBirr balance."""
+    if balance >= 1000: 
+        return "Premium" 
+    elif balance >= 500: 
+        return "Standard" 
+    return "Basic" 
+
+premium_count = 0
+standard_count = 0
+basic_count = 0
+
+
+
+for name, balance in customers: 
+    customer_tier = tier(balance)
+    print(f"{name}: {customer_tier} ({balance} ETB)") 
+    
+    if customer_tier == "Premium":
+        premium_count += 1
+    elif customer_tier == "Standard":
+        standard_count += 1
+    elif customer_tier == "Basic":
+        basic_count += 1
+
+
+print(f"Premium Customers: {premium_count}")
+print(f"Standard Customers: {standard_count}")
+print(f"Basic Customers: {basic_count}")
---
+++ practice.py
@@ -0,0 +1,41 @@
+temp_input = input("Enter a temperature in °C: ")
+temperature = int(temp_input)
+
+if temperature < 15:
+    print("cold")
+elif 15 <= temperature <= 28:
+    print("warm")
+else:
+    print("hot")
+
+print("-" * 20)  
+
+
+for number in range(1, 11):
+    print(f"Receipt #{number}")
+
+print("-" * 20)
+
+
+for num in range(1, 21):
+    if num % 2 == 0:
+        print(num)
+
+print("-" * 20)
+
+
+def apply_discount(price, percent=10):
+    discount_amount = price * (percent / 100)
+    return price - discount_amount
+
+
+print(f"Original 100, default 10% discount: {apply_discount(100)}")
+print(f"Original 100, custom 25% discount: {apply_discount(100, 25)}")
+
+print("-" * 20)
+
+count = 5
+while count > 0:
+    print(count)
+    count -= 1  
+print("Liftoff!")
